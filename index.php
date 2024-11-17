3e0850afdd08719f521eacb2afc1138e5fe1dd7f
samuelldmj <samuelldmj5@gmail.com>
2024-11-17 07:19:16 +0100

    some little changes in packages and files

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-//__dir__ used to obtain current code directory, that is the directory from which the code is being used.
+
+//__dir__ can be used to obtain current code directory, that is the directory from which the code is being used.
 require __DIR__ . '/vendor/autoload.php';
-
-require __DIR__ . '/src/helpers/header.inc.php';
-
+require __DIR__ . '/src/Helpers/header.inc.php';
+require __DIR__ . '/src/config/config.inc.php';
+require __DIR__ . "/src/config/database.inc.php";
 //require once would first look internally through the directory to check  if the file has been added. could be slower, but the impact is little.
-require __DIR__ .  '/src/routes/routes.php';
+require __DIR__ .  '/src/Routes/routes.php';
---
+++ readme.md
@@ -0,0 +1,8 @@
+#Library or packages used.
+
+## firebase jwt for api authentication
+## respect validation for input validation
+## ph-7 for nice and friendly response header
+## ramsey uuid for secure uuid
+## red bean 
+## vlucas for dontenv environment
