2d032416c47df15c378cdf04282baf31a0a736c3
samuelldmj <samuelldmj5@gmail.com>
2025-02-01 17:20:34 +0100

    item as the base class

--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -4,12 +4,10 @@ namespace Src\Entity;
 class Item implements Entitiable {
     private int $sequentialId;
     private string $itemName;
-
     private ?string $itemUuid = null;
-
-    private float $itemPrice;
-
-    private bool $itemAvailabilty;
+    private float $itemPrice = 0.0;
+    private bool $itemAvailabilty = false; 
+    private string $dateTime = ''; 
 
 
     public function setSequentialId(int $itemId): self{
@@ -57,6 +55,14 @@ public function getItemAvailabilty(): bool{
    return  $this->itemAvailabilty;
 }
 
+public function setCreatedDate(string $date ): self{
+    $this->dateTime = $date;
+    return $this;
+}
+
+public function getCreatedDate(): string{
+    return $this->dateTime;
+}
 
 public function unserialize(?array $data):self {
     
@@ -68,8 +74,8 @@ public function unserialize(?array $data):self {
         $this->setItemName($data['item_name']);
     }
 
-    if(!empty($data['item_price'])){
-        $this->setItemPrice($data['item_price']);
+    if(!empty($data['item_price_in_naira'])){
+        $this->setItemPrice($data['item_price_in_naira']);
     }
 
     if(!empty($data['item_availability'])){
@@ -78,6 +84,10 @@ public function unserialize(?array $data):self {
 
     if(!empty($data['item_uuid'])){
         $this->setItemUuid($data['item_uuid']);
+    }
+
+    if(!empty($data['create_date'])){
+        $this->setCreatedDate($data['create_date']);
     }
 
     return $this;
