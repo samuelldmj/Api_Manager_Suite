23d4a0f071b49fdcaff7aa1de66cc206e77c1717
samuelldmj <samuelldmj5@gmail.com>
2025-02-01 17:30:40 +0100

    Item Service interface logic for various action

---
+++ src/Service/ServiceInterface/ItemServiceInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Src\Service\ServiceInterface;
+
+interface ItemServiceInterface
+{
+    public function create(object $payload): array;
+    public function retrieve(string $id): array;
+    public function retrieveAll(): array;
+    public function update(string $id, object $payload): object|array;
+}
